9257d6282a1776a758105f1b1847e0f289763e17
Lacostezerq <evataniacamargodasilva274@gmail.com>
2024-09-11 23:43:30 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,1 @@
-function trocarpain(){document.querySelector("img").src="img/kaua.jpg"}function trocarausto(){document.querySelector("img").src="img/austo.jpg"}function trocarkaique(){document.querySelector("img").src="img/kaique.jpg"}
+function trocarpain(){document.querySelector("img").src="kaua.jpg"}function trocarausto(){document.querySelector("img").src="austo.jpg"}function trocarkaique(){document.querySelector("img").src="kaique.jpg"}
