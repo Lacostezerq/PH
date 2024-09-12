808a5a1888bef6b8b102161b9c1691221d584ded
Lacostezerq <evataniacamargodasilva274@gmail.com>
2024-09-11 23:34:45 -0300

    PH

---
+++ java.js
@@ -0,0 +1,1 @@
+function trocarpain(){document.querySelector("img").src="img/kaua.jpg"}function trocarausto(){document.querySelector("img").src="img/austo.jpg"}function trocarkaique(){document.querySelector("img").src="img/kaique.jpg"}
